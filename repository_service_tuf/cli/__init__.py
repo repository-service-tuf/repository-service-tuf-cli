61e6d02ef3a80221b144b1ec3a667c2806e5f36a
KAUTH <konpap1996@yahoo.com>
2022-10-13 20:27:58 +0200

    Add parameter for CLI to show version
    - We added the parameter ```--version```
    that shows the ```trs-cli``` version.
    
    - Added a check to make sure that the --version
    parameter exists and that it has the specified
    standard ouput format.
    
    Closes #70
    
    Signed-off-by: Konstantinos Papadopoulos <konpap1996@yahoo.com>

--- repository_service_tuf/cli/__init__.py
+++ repository_service_tuf/cli/__init__.py
@@ -1,13 +1,29 @@
 import importlib
 import os
 import pkgutil
+import re
 from pathlib import Path
 
 import rich_click as click  # type: ignore
 
 from repository_service_tuf import Dynaconf
+from repository_service_tuf.__version__ import version
 
 HOME = str(Path.home())
+
+# attempt to find the program name from pyproject.toml or give a default
+prog_name = "rstuf"
+try:
+    with open("pyproject.toml") as file:
+        for line in file:
+            match = re.search("repository_service_tuf.cli:rstuf", line)
+
+            if match:
+                prog_name = line.split("=")[0].strip()
+                break
+
+except FileNotFoundError:
+    pass
 
 
 @click.group(context_settings={"help_option_names": ["-h", "--help"]})
@@ -19,6 +35,8 @@ HOME = str(Path.home())
     help="Repository Service for TUF config file",
     required=False,
 )
+# adds the --version parameter
+@click.version_option(prog_name=prog_name, version=version)
 @click.pass_context
 def rstuf(context, config):
     """
