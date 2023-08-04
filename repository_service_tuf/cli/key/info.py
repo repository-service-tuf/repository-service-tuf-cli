cf495c98dd92b31c0d1fceb65e8123c7502ab940
Kairo de Araujo <kdearaujo@vmware.com>
2023-08-04 13:26:33 +0200

    Helper function to get key and remove private key
    - Add helper function to ask the key and load
    - Remove the private key option
    
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>

--- repository_service_tuf/cli/key/info.py
+++ repository_service_tuf/cli/key/info.py
@@ -1,42 +1,36 @@
 # SPDX-FileCopyrightText: 2022-2023 VMware Inc
 #
 # SPDX-License-Identifier: MIT
-
 from rich import prompt, table
 from rich.console import Console  # type: ignore
 
 from repository_service_tuf.cli import click
 from repository_service_tuf.cli.key import key
 from repository_service_tuf.constants import KeyType
-from repository_service_tuf.helpers.tuf import load_key
+from repository_service_tuf.helpers.tuf import RSTUFKey, load_key
 
 console = Console()
 
 
-@click.option(
-    "--show-private",
-    help=(
-        "Show the private key. WARNING: use private key information carefully."
-    ),
-    default=False,
-    show_default=True,
-    is_flag=True,
-)
-@key.command()
-def info(show_private: bool) -> None:
-    """Show key information"""
-
+def _get_key() -> RSTUFKey:
     keytype: str = prompt.Prompt.ask(
         "\nChoose key type",
         choices=KeyType.get_all_members(),
         default=KeyType.KEY_TYPE_ED25519.value,
     )
-
     filepath: str = prompt.Prompt.ask("Enter the key's [green]filename[/]")
+    password: str = click.prompt(
+        "Enter the private key password", hide_input=True
+    )
 
-    password = click.prompt("Enter the private key password", hide_input=True)
+    return load_key(filepath, keytype, password, "")
 
-    rstuf_key = load_key(filepath, keytype, password, "")
+
+@key.command()
+def info() -> None:
+    """Show key information"""
+
+    rstuf_key = _get_key()
     if rstuf_key.error:
         console.print(rstuf_key.error)
         raise click.ClickException("Failed to load the Key")
@@ -50,11 +44,6 @@ def info(show_private: bool) -> None:
         rstuf_key.key["keytype"],
         rstuf_key.key["keyval"]["public"],
     ]
-    if show_private:
-        key_table.add_column(
-            "Private Key", justify="center", style="red", no_wrap=True
-        )
-        row_items.append(rstuf_key.key["keyval"]["private"])
 
     key_table.add_row(*row_items)
 
--- tests/unit/cli/key/test_info.py
+++ tests/unit/cli/key/test_info.py
@@ -41,44 +41,6 @@ class TestKeyInfoInteraction:
             pretend.call("path/to/key.key", "ed25519", "password", "")
         ]
 
-    def test_info_show_private(self, client, test_context, monkeypatch):
-        input = [
-            "",  # Choose key type [ed25519/ecdsa/rsa] (ed25519):
-            "path/to/key.key",  # Enter the key's filename:
-            "password",  # Enter the private key password:
-        ]
-        monkeypatch.setattr(
-            info,
-            "load_key",
-            pretend.call_recorder(
-                lambda *a: RSTUFKey(
-                    key={
-                        "keyid": "keyid",
-                        "keytype": "keytype",
-                        "keyval": {
-                            "public": "k_public",
-                            "private": "k_private",
-                        },
-                    }
-                )
-            ),
-        )
-
-        result = client.invoke(
-            info.info,
-            "--show-private",
-            input="\n".join(input),
-            obj=test_context,
-        )
-        assert result.exit_code == 0, result.output
-        assert "keyid" in result.output
-        assert "keytype" in result.output
-        assert "k_public" in result.output
-        assert "k_private" in result.output
-        assert info.load_key.calls == [
-            pretend.call("path/to/key.key", "ed25519", "password", "")
-        ]
-
     def test_info_failed_load_key(self, client, test_context, monkeypatch):
         input = [
             "",  # Choose key type [ed25519/ecdsa/rsa] (ed25519):
