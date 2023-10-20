b01bf93c500d7ff1c5502c8078013d6ed4e836aa
Martin Vrachev <mvrachev@vmware.com>
2023-10-20 21:11:42 +0300

    Add the choice of a key scheme as a requirement
    When describing a public key during bootstrap the administrators will
    now have to describe the public key scheme as well.
    
    For that reason, I have also updated the "rstuf key info" command and
    now it gives information about key scheme as well.
    
    Signed-off-by: Martin Vrachev <mvrachev@vmware.com>

--- repository_service_tuf/constants.py
+++ repository_service_tuf/constants.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MIT
 
 from enum import Enum
-from typing import List
+from typing import Dict, List
 
 from securesystemslib.interface import (  # type: ignore
     KEY_TYPE_ECDSA,
@@ -22,3 +22,13 @@ class KeyType(str, Enum):
     @classmethod
     def get_all_members(cls) -> List[str]:
         return [e.value for e in cls]
+
+
+# Those defaults are taken from scheme arg default at
+# import_KEY_TYPE_privatekey_from_file() functions at
+# securesystemslib.interface.py
+SCHEME_DEFAULTS: Dict[str, str] = {
+    KeyType.KEY_TYPE_ED25519.value: KEY_TYPE_ED25519,
+    KeyType.KEY_TYPE_ECDSA.value: "ecdsa-sha2-nistp256",
+    KeyType.KEY_TYPE_RSA.value: "rsassa-pss-sha256",
+}
