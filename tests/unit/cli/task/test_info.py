d9b48d11f20f711d6ae66354a7fc6b5a40af971e
Konstantinos Papadopoulos <konpap1996@yahoo.com>
2023-10-22 15:39:58 +0200

    fixup! Add unit tests for CLI task info command

--- tests/unit/cli/task/test_info.py
+++ tests/unit/cli/task/test_info.py
@@ -16,13 +16,15 @@ class TestTaskInfoInteraction:
 
         task_id = "0123456789"
 
-        input = [
+        input_steps = [
+            "--api-server",
+            "http://127.0.0.1",
             task_id,
         ]
 
         info.task_status = pretend.call_recorder(lambda *a, **kw: "123")
 
-        result = client.invoke(info.info, input, obj=test_context)
+        result = client.invoke(info.info, input_steps, obj=test_context)
 
         assert info.task_status.calls == [
             pretend.call(
@@ -33,3 +35,23 @@ class TestTaskInfoInteraction:
         ]
 
         assert result.exit_code == 0, result.output
+
+    def test_info_no_api_server_missing_param(self, client, test_context):
+        """
+        Test that the task info command checks if a server URL has been
+        set/configured.
+        """
+
+        task_id = "0123456789"
+
+        input_steps = [
+            task_id,
+        ]
+
+        result = client.invoke(info.info, input_steps, obj=test_context)
+        output_message = (
+            "Requires '--api-server' or configuring the `.rstuf.yml` file."
+        )
+
+        assert result.exit_code == 1
+        assert output_message in result.output
