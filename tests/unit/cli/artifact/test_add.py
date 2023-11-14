878432010d8b24b7257eaeb5ac16500574d86240
Kairo de Araujo <kdearaujo@vmware.com>
2023-11-14 13:48:00 +0100

    Add extra test to add without path
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>

--- tests/unit/cli/artifact/test_add.py
+++ tests/unit/cli/artifact/test_add.py
@@ -26,9 +26,6 @@ class TestAddArtifactInteraction:
             "fake-server",
         ]
 
-        add.create_artifact_payload_from_filepath = pretend.call_recorder(
-            lambda *a, **kw: {"k": "v"}
-        )
         add.send_payload = pretend.call_recorder(lambda *a, **kw: "123")
 
         with client.isolated_filesystem():
@@ -41,15 +38,77 @@ class TestAddArtifactInteraction:
         assert "Successfully submitted task" in result.output
         assert "RSTUF task ID" in result.output
 
-        assert add.create_artifact_payload_from_filepath.calls == [
-            pretend.call(filepath=artifact_path, path=path)
+        assert add.send_payload.calls == [
+            pretend.call(
+                settings=test_context["settings"],
+                url=URL.ARTIFACTS.value,
+                payload={
+                    "targets": [
+                        {
+                            "info": {
+                                "length": 14,
+                                "hashes": {
+                                    "blake2b-256": "5b23eadf78d64e16f4ecf121e6631c68fa8eb64fcd5a0762fd36ef37f61369e9"  # noqa
+                                },
+                                "custom": None,
+                            },
+                            "path": f"{path}/{artifact_path}",
+                        }
+                    ],
+                    "add_task_id_to_custom": False,
+                    "publish_targets": True,
+                },
+                expected_msg="New Artifact(s) successfully submitted.",
+                command_name="Artifact Addition",
+            )
         ]
+
+    def test_without_path(self, client, test_context):
+        """
+        Test that the add artifact command works as expected given the
+        expected arguments/options in the CLI.
+        """
+
+        artifact_path = "dummy-artifact"
+
+        input = [
+            artifact_path,  # artifact filepath
+            "--api-server",
+            "fake-server",
+        ]
+
+        add.send_payload = pretend.call_recorder(lambda *a, **kw: "123")
+
+        with client.isolated_filesystem():
+            with open(artifact_path, "w") as f:
+                f.write("Dummy Artifact")
+
+            result = client.invoke(add.add, input, obj=test_context)
+
+        assert result.exit_code == 0, result.output
+        assert "Successfully submitted task" in result.output
+        assert "RSTUF task ID" in result.output
 
         assert add.send_payload.calls == [
             pretend.call(
                 settings=test_context["settings"],
                 url=URL.ARTIFACTS.value,
-                payload={"k": "v"},
+                payload={
+                    "targets": [
+                        {
+                            "info": {
+                                "length": 14,
+                                "hashes": {
+                                    "blake2b-256": "5b23eadf78d64e16f4ecf121e6631c68fa8eb64fcd5a0762fd36ef37f61369e9"  # noqa
+                                },
+                                "custom": None,
+                            },
+                            "path": artifact_path,
+                        }
+                    ],
+                    "add_task_id_to_custom": False,
+                    "publish_targets": True,
+                },
                 expected_msg="New Artifact(s) successfully submitted.",
                 command_name="Artifact Addition",
             )
