0f41944d682c1f5023b627e22ea3225db25863ab
Lukas Puehringer <lukas.puehringer@nyu.edu>
2024-01-30 16:15:55 +0100

    Misc admin2 sign style, refactor and improvements
    - Polish wording
    - Use markdown formatting for cli title
    - Use tables to display metadata to be signed (for review) and missing
      key information (to choose a key for signing)
    
    The most notable refactor in this patch changes the signature
    verification helpers to better separate data and presentation.
    The new code also aligns with the planned upstream interface changes.
    
    The "choose key for signing" prompt is improved to allow entering keyids
    and key names.
    
    **Note about testing**
    With the more complex presentation, the previous console input/output
    test design becomes infeasible.
    This patch replaces previous tests and related helpers, with a single
    smoke test, which checks, if given the correct inputs, the tool indeed
    creates the expected signature.
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- repository_service_tuf/cli/admin2.py
+++ repository_service_tuf/cli/admin2.py
@@ -2,33 +2,26 @@
 
 Provides alternative ceremony, metadata update, and sign admin cli commands.
 
-Goals
------
-- use state-of-the-art securesystemslib Signer API only
-- simplify (e.g. avoid custom/redundant abstractions over Metadata API)
-- configure online signer location via uri attached to public key
-  (for repository-service-tuf/repository-service-tuf-worker#427)
-
 TODO
 ----
 - implement ceremony
 - implement update
-- polish enough so that reviewers can try it out
-- Integrate with existing admin cli
 
 """
 import time
-from typing import Any, Dict, Optional, Tuple
+from dataclasses import dataclass
+from typing import Any, Dict, List, Optional, Tuple
 
 import click
 from click import ClickException
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from requests import request
 from requests.exceptions import RequestException
-from rich.pretty import pprint
+from rich.markdown import Markdown
 from rich.prompt import Prompt
+from rich.table import Table
 from securesystemslib.signer import CryptoSigner, Key, Signature, Signer
-from tuf.api.metadata import Metadata, Root, UnsignedMetadataError
+from tuf.api.metadata import Metadata, Root, Timestamp, UnsignedMetadataError
 
 from repository_service_tuf.cli import console, rstuf
 from repository_service_tuf.helpers.api_client import URL as ROUTE
@@ -36,70 +29,65 @@ from repository_service_tuf.helpers.api_client import URL as ROUTE
 
 def _load_signer(public_key: Key) -> Signer:
     """Ask for details to load signer, load and return."""
-    # TODO: Give choice -> hsm, sigstore, ...
-
-    # TODO: clarify supported key types, format
-    path = Prompt.ask("Enter path to encrypted local private key")
+    path = Prompt.ask("Please enter path to encrypted local private key")
 
     with open(path, "rb") as f:
         private_pem = f.read()
 
-    password = Prompt.ask("Enter password", password=True)
+    password = Prompt.ask("Please enter password", password=True)
     private_key = load_pem_private_key(private_pem, password.encode())
     return CryptoSigner(private_key, public_key)
 
 
+@dataclass
+class VerificationResult:
+    """tuf.api.metadata.VerificationResult but with Keys objects
+
+    Likely upstreamed (theupdateframework/python-tuf#2544)
+    """
+
+    verified: bool
+    signed: Dict[str, Key]
+    unsigned: Dict[str, Key]
+    threshold: int
+
+
 def _get_verification_result(
     delegator: Root, delegate: Metadata[Root]
-) -> Tuple[Dict[str, Key], str]:
-    """Get opinionated verification result.
-
-    TODO: consider upstreaming features to `get_verification_result`:
-    - return keys (e.g. as dict), not keyids!
-    - missing signature count is convenient, but not necessary
-    - could also just return threshold
-    (IIRC threshold was removed from result, because it can't be unioned.
-     Maybe threshold in the result is more useful, than the union method.)
-
-    Returns dict of unused keys and a message, to tell how many signatures are
-    missing and from which keys. Empty message means fully signed.
-    """
+) -> VerificationResult:
+    """Return signature verification result for delegate."""
     result = delegator.get_verification_result(
         Root.type, delegate.signed_bytes, delegate.signatures
     )
-    msg = ""
-    if not result.verified:
-        missing = delegator.roles[Root.type].threshold - len(result.signed)
-        msg = (
-            f"need {missing} signature(s) from any of "
-            f"{sorted(result.unsigned)}"
-        )
+    signed = {keyid: delegator.get_key(keyid) for keyid in result.signed}
+    unsigned = {keyid: delegator.get_key(keyid) for keyid in result.unsigned}
 
-    unused_keys = {
-        keyid: delegator.get_key(keyid) for keyid in result.unsigned
-    }
+    threshold = delegator.roles[Root.type].threshold
 
-    return unused_keys, msg
+    return VerificationResult(result.verified, signed, unsigned, threshold)
 
 
-def _get_combined_verification_result(
-    metadata: Metadata[Root], prev_root: Optional[Root]
-) -> Tuple[Dict[str, Key], str]:
-    """Get combined verification results from previous and current root."""
-    unused_keys, missing_sig_msg = _get_verification_result(
-        metadata.signed, metadata
-    )
-    if prev_root:
-        prev_keys, prev_msg = _get_verification_result(prev_root, metadata)
-        unused_keys.update(prev_keys)
+def _show_missing_signatures(
+    result: VerificationResult, prev_result: Optional[VerificationResult]
+) -> None:
+    results_to_show = [result]
+    if prev_result:
+        if (
+            prev_result.signed != result.signed
+            or prev_result.threshold != result.threshold
+        ):
+            results_to_show.append(prev_result)
 
-        # Combine "missing signatures" messages from old and new root:
-        # - show only non-empty message (filter)
-        # - show only one message, if both are equal (set)
-        missing_sig_msg = "\n".join(
-            filter(None, sorted({missing_sig_msg, prev_msg}))
-        )
-    return unused_keys, missing_sig_msg
+    for result in results_to_show:
+        missing = result.threshold - len(result.signed)
+        title = f"Please add {missing} more signature(s) from any of "
+        key_table = Table("ID", "Name", title=title)
+
+        for keyid, key in result.unsigned.items():
+            name = key.unrecognized_fields.get("name")
+            key_table.add_row(keyid, name)
+
+        console.print(key_table)
 
 
 def _sign_one(
@@ -110,20 +98,28 @@ def _sign_one(
     Return None, if metadata is already fully missing.
     Otherwise, loop until success and returns the added signature.
     """
-    unused_keys, missing_sig_msg = _get_combined_verification_result(
-        metadata, prev_root
-    )
-    if not missing_sig_msg:
-        console.print("Metadata fully signed.")
+    result = _get_verification_result(metadata.signed, metadata)
+    keys_to_use = {}
+    if not result.verified:
+        keys_to_use = result.unsigned
+
+    prev_result = None
+    if prev_root:
+        prev_result = _get_verification_result(prev_root, metadata)
+        if not prev_result.verified:
+            keys_to_use.update(prev_result.unsigned)
+
+    if not keys_to_use:
+        console.print("Metadata is fully signed.")
         return None
 
     _show(metadata.signed)
-    console.print(missing_sig_msg)
+    _show_missing_signatures(result, prev_result)
 
     # Loop until success
     signature = None
     while not signature:
-        signature = _sign(metadata, unused_keys)
+        signature = _sign(metadata, keys_to_use)
 
     return signature
 
@@ -134,21 +130,67 @@ def _sign(metadata: Metadata, keys: Dict[str, Key]) -> Optional[Signature]:
     Return Signature or None, if signing fails.
     """
     signature = None
-    keyid = Prompt.ask("Choose key", choices=sorted(keys))
+    # TODO: Make sure keyid / name is not truncated in key table.
+    # TODO: Check name collision?
+    # TODO: Support keyid prefix?
+    # -> Then we'd also need to check for collision. Or, should we just enforce
+    #    adding mandatory unique names in bootstrap/update cli, and use full
+    #    keyids as fallback?
+    choices = {}
+    for keyid, key in keys.items():
+        choices[keyid] = key
+        if name := key.unrecognized_fields.get("name"):
+            choices[name] = key
+
+    choice = Prompt.ask(
+        "Please choose signing key by entering keyid or name",
+        choices=choices,
+        show_choices=False,
+    )
+    key = choices[choice]
     try:
-        signer = _load_signer(keys[keyid])
+        signer = _load_signer(key)
         signature = metadata.sign(signer, append=True)
-        console.print(f"Signed with key {keyid}")
+        console.print(f"Signed metadata with key '{choice}'")
 
     except (ValueError, OSError, UnsignedMetadataError) as e:
-        console.print(f"Cannot sign: {e}")
+        console.print(f"Cannot sign metadata with key '{choice}': {e}")
 
     return signature
 
 
+def _get_root_keys(root: Root) -> List[Key]:
+    return [root.get_key(keyid) for keyid in root.roles[Root.type].keyids]
+
+
+def _get_online_key(root: Root) -> Key:
+    # TODO: assert all online roles have the same and only one keyid
+    return root.get_key(root.roles[Timestamp.type].keyids[0])
+
+
 def _show(root: Root):
     """Pretty print root metadata."""
-    pprint(root.to_dict())
+
+    key_table = Table("Role", "ID", "Name", "Signing Scheme", "Public Value")
+    for key in _get_root_keys(root):
+        public_value = key.keyval["public"]  # SSlibKey-specific
+        name = key.unrecognized_fields.get("name")
+        key_table.add_row("Root", key.keyid, name, key.scheme, public_value)
+    key = _get_online_key(root)
+    key_table.add_row(
+        "Online", key.keyid, "", key.scheme, key.keyval["public"]
+    )
+
+    root_table = Table("Infos", "Keys", title="Root Metadata")
+    root_table.add_row(
+        (
+            f"Expiration: {root.expires:%x}\n"
+            f"Threshold: {root.roles[Root.type].threshold}"
+        ),
+        key_table,
+    )
+
+    console.print(root_table)
 
 
 def _request(method: str, url: str, **kwargs: Any) -> Dict[str, Any]:
@@ -233,7 +275,7 @@ def admin2():
 )
 def sign(api_server: str) -> None:
     """Add one signature to root metadata."""
-    console.print("Sign")
+    console.print("\n", Markdown("# Metadata Signing Tool"))
 
     try:
         root_md, prev_root = _fetch_metadata(api_server)
--- tests/unit/cli/test_admin2.py
+++ tests/unit/cli/test_admin2.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 
+import pretend
 import pytest
 from click.testing import CliRunner
 from rich.console import Console
 from rich.pretty import pprint
 from tuf.api.metadata import Metadata, Root
 
+from repository_service_tuf.cli import admin2
 from repository_service_tuf.cli.admin2 import sign
 
 _FILES = Path(__file__).parent.parent.parent / "files"
@@ -33,110 +35,25 @@ def patch_getpass(monkeypatch):
 
 # flake8: noqa
 class TestSign:
-    @staticmethod
-    def _pretty(arg):
-        """Helper to format a string as pprint would do it."""
-        console = Console()
-        with console.capture() as capture:
-            pprint(arg, console=console)
-        return capture.get()
+    def test_sign_v2(self, client: CliRunner, monkeypatch, patch_getpass):
+        signer_keyid = (
+            "50d7e110ad65f3b2dba5c3cfc8c5ca259be9774cc26be3410044ffd4be3aa5f3"
+        )
+        root1 = Metadata[Root].from_file(f"{_ROOTS / 'v1.json'}")
+        root2 = Metadata[Root].from_file(f"{_ROOTS / 'v2.json'}")
 
-    @staticmethod
-    def _split(dialog):
-        """Helper to split (output, input) tuples of lines in a prompt dialog.
-        Some lines may only have outputs."""
+        inputs = [signer_keyid, f"{_PEMS / 'ec'}"]
 
-        outputs = inputs = ""
-        for line in dialog:
-            assert isinstance(line, tuple) and len(line) in [1, 2]
-            outputs += line[0]
-            if len(line) == 2:
-                inputs += line[1] + "\n"
+        mock_api = pretend.call_recorder(lambda x, y: None)
+        monkeypatch.setattr(
+            admin2, "_fetch_metadata", lambda x: (root2, root1.signed)
+        )
+        monkeypatch.setattr(admin2, "_push_signature", mock_api)
 
-        return outputs, inputs
+        assert not root2.signatures
+        result = client.invoke(
+            sign, "--api-server mocked", input="\n".join(inputs)
+        )
 
-    def test_sign_v1(self, client: CliRunner, patch_getpass):
-        """Sign root v1, with 2/2 keys."""
-
-        root = Metadata[Root].from_file(f"{_ROOTS / 'v1.json'}")
-        root_pretty = self._pretty(root.signed.to_dict())
-
-        dialog = [
-            (
-                "Enter path to root to sign: ",
-                f"{_ROOTS / 'v1.json'}",
-            ),
-            (root_pretty,),
-            (
-                "need 2 signature(s) from any of ['50d7e110ad65f3b2dba5c3cfc8c5ca259be9774cc26be3410044ffd4be3aa5f3', 'c6d8bf2e4f48b41ac2ce8eca21415ca8ef68c133b47fc33df03d4070a7e1e9cc']",
-            ),
-            (
-                "Choose key [50d7e110ad65f3b2dba5c3cfc8c5ca259be9774cc26be3410044ffd4be3aa5f3/c6d8bf2e4f48b41ac2ce8eca21415ca8ef68c133b47fc33df03d4070a7e1e9cc]: ",
-                "50d7e110ad65f3b2dba5c3cfc8c5ca259be9774cc26be3410044ffd4be3aa5f3",
-            ),
-            (
-                "Enter path to encrypted local private key: ",
-                f"{_PEMS / 'ec'}",
-            ),
-            ("Enter password: ",),  # provided via patch_getpass
-            (
-                "Signed with key 50d7e110ad65f3b2dba5c3cfc8c5ca259be9774cc26be3410044ffd4be3aa5f3",
-            ),
-            (
-                "Save? [y/n]: ",
-                "n",
-            ),
-            ("Bye.",),
-        ]
-
-        outputs, inputs = self._split(dialog)
-        result = client.invoke(sign, input=inputs)
-        # ignore newlines to make life easier (rich inserts plenty)
-        assert outputs.replace("\n", "") == result.output.replace("\n", "")
-
-    def test_sign_v2(self, client, patch_getpass):
-        """Sign root v2, with 2/2 keys from old root and 2/2 from new, where
-        1 key is in both old and new (needs 3 signatures in total)."""
-
-        root = Metadata[Root].from_file(f"{_ROOTS / 'v2.json'}")
-        root_pretty = self._pretty(root.signed.to_dict())
-
-        dialog = [
-            (
-                "Enter path to root to sign: ",
-                f"{_ROOTS / 'v2.json'}",
-            ),
-            (
-                "Enter path to previous root: ",
-                f"{_ROOTS / 'v1.json'}",
-            ),
-            (root_pretty,),
-            (
-                "need 2 signature(s) from any of ['2f685fa7546f1856b123223ab086b3def14c89d24eef18f49c32508c2f60e241', '50d7e110ad65f3b2dba5c3cfc8c5ca259be9774cc26be3410044ffd4be3aa5f3']",
-            ),
-            (
-                "need 2 signature(s) from any of ['50d7e110ad65f3b2dba5c3cfc8c5ca259be9774cc26be3410044ffd4be3aa5f3', 'c6d8bf2e4f48b41ac2ce8eca21415ca8ef68c133b47fc33df03d4070a7e1e9cc']",
-            ),
-            (
-                "Choose key [2f685fa7546f1856b123223ab086b3def14c89d24eef18f49c32508c2f60e241/50d7e110ad65f3b2dba5c3cfc8c5ca259be9774cc26be3410044ffd4be3aa5f3/c6d8bf2e4f48b41ac2ce8eca21415ca8ef68c133b47fc33df03d4070a7e1e9cc]: ",
-                "50d7e110ad65f3b2dba5c3cfc8c5ca259be9774cc26be3410044ffd4be3aa5f3",
-            ),
-            (
-                "Enter path to encrypted local private key: ",
-                f"{_PEMS / 'ec'}",
-            ),
-            ("Enter password: ",),  # provided via patch_getpass
-            (
-                "Signed with key 50d7e110ad65f3b2dba5c3cfc8c5ca259be9774cc26be3410044ffd4be3aa5f3",
-            ),
-            (
-                "Save? [y/n]: ",
-                "n",
-            ),
-            ("Bye.",),
-        ]
-
-        outputs, inputs = self._split(dialog)
-        result = client.invoke(sign, input=inputs)
-        # ignore newlines to make life easier (rich inserts plenty)
-        assert outputs.replace("\n", "") == result.output.replace("\n", "")
+        # Assert signature was added to metadata and sent to mock API
+        assert root2.signatures[signer_keyid] == mock_api.calls[0].args[1]
