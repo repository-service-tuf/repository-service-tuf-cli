abda770f80593bb375f80ab84f23a2afe21bb115
Lukas Puehringer <lukas.puehringer@nyu.edu>
2024-02-08 15:49:14 +0100

    Add alternative metadata update cli
    *depends on #477*
    
    Invoke as `rstuf admin2 update`
    
    Provides root metadata update dialog similar to the existing `rstuf
    admin metadata update`. Users can change expiry date, root keys +
    threshold, and the online key, and re-sign the updated root metadata.
    
    **Advantages compared to existing tool**
    
    - Better separates public and private key management. This allows
      metadata update regardless of private key availability (needed for
      DAS), and sets the stage for other key technologies (e.g. HSM, cloud
      KMS, etc.)
    - Introduces signer URIs, included with the online public key. This
      makes configuration of arbitrary online signing providers (e.g. cloud
      KMS) substantially easier (see option 2 in
      repository-service-tuf/repository-service-tuf#580).
    - Uses only state-of-the-art securesystemslib API
    - Re-uses signing dialog from alternative sign cli (#477)
    - Is designed for re-use in `rstuf admin2 bootstrap`
    - Omits redundant data model on top of existing TUF Metadata API model.
    
    **Why not update the existing tool?**
    
    Above all, the strong coupling between public and private key
    management, tied to the legacy securesystemslib API, would have required
    rather invasive changes. A rewrite seemed more feasible. That said, I
    took a lot of inspiration from the existing tool, and highly value all
    the related efforts.
    
    *Follow-up work**
    
    - API integration
    - Polish wording and visual
      - Should be consistent, informative, succinct
      - Most importantly, how much public key detail is needed when?
    - Maybe polish flow
    - Improve test coverage and tests
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- repository_service_tuf/cli/admin2.py
+++ repository_service_tuf/cli/admin2.py
@@ -2,14 +2,28 @@
 
 Provides alternative ceremony, metadata update, and sign admin cli commands.
 
+Goals
+-----
+- use state-of-the-art securesystemslib Signer API only
+- simplify (e.g. avoid custom/redundant abstractions over Metadata API)
+- configure online signer location via uri attached to public key
+  (for repository-service-tuf/repository-service-tuf-worker#427)
+
 TODO
 ----
+- finalize update
+  - beautify (see pr comments)
+  - api integration
+  - cli options to save payload, or send payload only
+  - assert one valid signature before pushing / saving
+
 - implement ceremony
-- implement update
 
 """
 
 import time
+from copy import deepcopy
+from datetime import datetime, timedelta
 from typing import Any, Dict, List, Optional, Tuple
 
 import click
@@ -17,23 +31,276 @@ import click
 # Magic import to unbreak `load_pem_private_key` - pyca/cryptography#10315
 import cryptography.hazmat.backends.openssl.backend  # noqa: F401
 from click import ClickException
-from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from cryptography.hazmat.primitives.serialization import (
+    load_pem_private_key,
+    load_pem_public_key,
+)
 from requests import request
 from requests.exceptions import RequestException
 from rich.markdown import Markdown
-from rich.prompt import Prompt
+from rich.prompt import Confirm, IntPrompt, InvalidResponse, Prompt
 from rich.table import Table
-from securesystemslib.signer import CryptoSigner, Key, Signature, Signer
+from securesystemslib.exceptions import StorageError
+from securesystemslib.signer import (
+    CryptoSigner,
+    Key,
+    Signature,
+    Signer,
+    SSlibKey,
+)
 from tuf.api.metadata import (
     Metadata,
     Root,
     RootVerificationResult,
+    Snapshot,
+    Targets,
     Timestamp,
     UnsignedMetadataError,
 )
+from tuf.api.serialization import DeserializationError
 
+# TODO: Should we use the global rstuf console exclusively? We do use it for
+# `console.print`, but not with `Confirm/Prompt.ask`. The latter uses a default
+# console from `rich`. Using a single console everywhere would makes custom
+# configuration or, more importantly, patching in tests easier:
+# https://rich.readthedocs.io/en/stable/console.html#console-api
+# https://rich.readthedocs.io/en/stable/console.html#capturing-output
 from repository_service_tuf.cli import console, rstuf
 from repository_service_tuf.helpers.api_client import URL as ROUTE
+
+ONLINE_ROLE_NAMES = {Timestamp.type, Snapshot.type, Targets.type}
+
+KEY_URI_FIELD = "x-rstuf-online-key-uri"
+# TODO: Consider using "x-rstuf-" prefix
+KEY_NAME_FIELD = "name"
+
+# Use locale's appropriate date representation to display the expiry date.
+EXPIRY_FORMAT = "%x"
+
+
+class _PositiveIntPrompt(IntPrompt):
+    validate_error_message = (
+        "[prompt.invalid]Please enter a valid positive integer number"
+    )
+
+    def process_response(self, value: str) -> int:
+        return_value: int = super().process_response(value)
+        if return_value < 1:
+            raise InvalidResponse(self.validate_error_message)
+        return return_value
+
+
+def _load_public_key_from_file() -> Key:
+    """Prompt for path to local public key, load and return."""
+
+    path = Prompt.ask("Please enter a public key path")
+    with open(path, "rb") as f:
+        public_pem = f.read()
+
+    crypto = load_pem_public_key(public_pem)
+
+    key = SSlibKey.from_crypto(crypto)
+
+    return key
+
+
+def _show_root_key_info(root: Root) -> None:
+    """Pretty print root keys and threshold."""
+    # TODO: Make prettier and useful
+    root_role = root.get_delegated_role(Root.type)
+    console.print(f"Current Threshold: {root_role.threshold}")
+    console.print("Current Keys:")
+    for keyid in root_role.keyids:
+        key = root.get_key(keyid)
+        name = key.unrecognized_fields.get(KEY_NAME_FIELD, "")
+        if name:
+            name = f"(name: {name})"
+        console.print(f"keyid: {keyid}", name)
+
+
+def _add_root_keys(root: Root) -> None:
+    """Prompt loop to add root keys.
+
+    Loops until user exit and threshold is met."""
+
+    root_role = root.get_delegated_role(Root.type)
+
+    while True:
+        missing = root_role.threshold - len(root_role.keyids)
+        if missing > 0:
+            console.print(
+                f"Please add {missing} key(s) "
+                f"to meet threshold ({root_role.threshold})"
+            )
+        else:
+            if not Confirm.ask("Do you want to add a root key?"):
+                break
+
+        try:
+            new_key = _load_public_key_from_file()
+        except (OSError, ValueError) as e:
+            console.print(f"Cannot load: {e}")
+            continue
+
+        # TODO: Handle cases where key already exists:
+        #  * If the key exists as root key the key object remains unchanged
+        #    in the metadata keystore and a name change will not take effect.
+        #  * If the key is already used as online key, the keyid is
+        #    added to root keyids, but the key object also remains unchanged.
+        # Lets just disallow re-adding a key here, even if it is for a
+        # different role, given that different roles have different extra key
+        # fields (root's "KEY_NAME_FIELD" vs. online roles' "KEY_URI_FIELD")
+
+        # TODO: Should name be mandatory? Would be helpful to distinguish keys
+        name = Prompt.ask(
+            "Please enter a key name, "
+            "or press enter to continue without name",
+            show_default=False,
+        )
+        if name:
+            new_key.unrecognized_fields[KEY_NAME_FIELD] = name
+
+        root.add_key(new_key, Root.type)
+        console.print(f"Added root key '{new_key.keyid}'")
+
+        _show_root_key_info(root)
+
+
+def _remove_root_keys(root: Root) -> None:
+    """Prompt loop to remove root keys.
+
+    Loops until no keys left or user exit (threshold is ignored)."""
+    root_role = root.get_delegated_role(Root.type)
+
+    while True:
+        if not root_role.keyids:
+            break
+
+        if not Confirm.ask("Do you want to remove a root key?"):
+            break
+
+        keyid = Prompt.ask(
+            "Choose key to remove", choices=sorted(root_role.keyids)
+        )
+        root.revoke_key(keyid, Root.type)
+        console.print(f"Removed root key '{keyid}'")
+
+        _show_root_key_info(root)
+
+
+def _configure_root_keys(root: Root) -> None:
+    """Prompt series with loop to add/remove root keys, and enter threshold.
+
+    Loops until user exit (at least one root key must be set).
+
+    """
+    console.print("Root Key Configuration")
+
+    # Get current keys
+    root_role = root.get_delegated_role(Root.type)
+
+    while True:
+        _show_root_key_info(root)
+
+        # Allow user to skip offline key change (assumes valid metadata)
+        if not Confirm.ask("Do you want to change root keys?"):
+            break
+
+        # TODO: Should we ask for threshold *after* removing/adding keys?
+        # Currently, if the users accidentally sets a wrong threshold, they
+        # might have a hard time to add enough keys to meet it, which they have
+        # to do before they can correct the mistake.
+        if Confirm.ask("Do you want to change the root signature threshold?"):
+            root_role.threshold = _PositiveIntPrompt.ask(
+                "Please enter root signature threshold"
+            )
+            console.print(
+                f"Changed root signature threshold to {root_role.threshold}"
+            )
+
+        # Allow removing keys, even if we drop below threshold.
+        _remove_root_keys(root)
+
+        # Require enough keys to meet the threshold
+        _add_root_keys(root)
+
+
+def _configure_online_key(root: Root) -> None:
+    """Prompt loop to change online key.
+
+    Loops until user exit.
+    """
+    console.print("Online Key Configuration")
+
+    while True:
+        # Get current key
+        # TODO: Assert that all online roles have same/single keyid. Mandate
+        # change otherwise.
+        ts_role = root.get_delegated_role(Timestamp.type)
+        current_key = root.get_key(ts_role.keyids[0])
+
+        # Show key
+        # TODO: Make pretty and useful
+        console.print("Current Key:")
+        uri = current_key.unrecognized_fields.get(KEY_URI_FIELD, "")
+        if uri:
+            uri = f"(uri: '{uri}')"
+        console.print(f"keyid: {current_key.keyid}", uri)
+
+        # Allow user to skip online key change (assumes valid metadata)
+        if not Confirm.ask("Do you want to change the online key?"):
+            break
+
+        # Load new key
+        try:
+            new_key = _load_public_key_from_file()
+
+        except (OSError, ValueError) as e:
+            console.print(f"Cannot load: {e}")
+            continue
+
+        # For file-based keys we default to a "relative file path uri" using
+        # keyid as filename. The online signing key must be made available to
+        # the worker under that filename. Additionally, a base path to the file
+        # can be specified via container configuration.
+        # see repository-service-tuf/repository-service-tuf#580 for details
+        uri = f"fn:{new_key.keyid}"
+
+        new_key.unrecognized_fields[KEY_URI_FIELD] = uri
+
+        # Remove current and add new key
+        for role_name in ONLINE_ROLE_NAMES:
+            root.revoke_key(current_key.keyid, role_name)
+
+            # TODO: Handle case where key already exists as root key. Disallow?
+            root.add_key(new_key, role_name)
+
+        console.print(f"Configured online key: '{new_key.keyid}'")
+
+
+def _configure_expiry(root: Root) -> None:
+    """Prompt loop to configure root expiry.
+
+    Loops until user exit and metadata is not expired.
+    """
+    console.print("Expiration Date Configuration")
+
+    while True:
+        if root.is_expired():
+            console.print(
+                f"Root has expired on {root.expires:{EXPIRY_FORMAT}}"
+            )
+        else:
+            console.print(f"Root expires on {root.expires:{EXPIRY_FORMAT}}")
+            if not Confirm.ask("Do you want to change the expiry date?"):
+                break
+
+        days = _PositiveIntPrompt.ask(
+            "Please enter number of days from now, when root should expire"
+        )
+
+        root.expires = datetime.utcnow() + timedelta(days=days)
+        console.print(f"Changed root to expire in {days} days")
 
 
 def _load_signer(public_key: Key) -> Signer:
@@ -63,10 +330,63 @@ def _show_missing_signatures(results: RootVerificationResult) -> None:
         key_table = Table("ID", "Name", title=title)
 
         for keyid, key in result.unsigned.items():
-            name = key.unrecognized_fields.get("name")
+            name = key.unrecognized_fields.get(KEY_NAME_FIELD)
             key_table.add_row(keyid, name)
 
         console.print(key_table)
+
+
+def _get_missing_keys(results: RootVerificationResult) -> dict[str, Key]:
+    missing_keys = {}
+    # Use only those keys to sign, whose signatures are effectively missing.
+    # This also means the cli can't be used to sign beyond the threshold.
+    if not results.first.verified:
+        missing_keys.update(results.first.unsigned)
+
+    if not results.second.verified:
+        missing_keys.update(results.second.unsigned)
+
+    return missing_keys
+
+
+def _sign_multiple(
+    metadata: Metadata[Root],
+    prev_root: Optional[Root],
+) -> Optional[list[Signature]]:
+    """Prompt loop to add signatures to root.
+
+    Prints metadata for review once, and signature requirements.
+    Loops until fully signed or user exit.
+    """
+    signatures: List[Signature] = []
+    show_metadata = True
+    while True:
+        results = metadata.signed.get_root_verification_result(
+            prev_root,
+            metadata.signed_bytes,
+            metadata.signatures,
+        )
+
+        if results.verified:
+            console.print("Metadata is fully signed.")
+            return signatures
+
+        # Show metadata for review once
+        if show_metadata:
+            _show(metadata.signed)
+            show_metadata = False
+
+        _show_missing_signatures(results)
+        missing_keys = _get_missing_keys(results)
+        # Loop until signing success or user exit
+        while True:
+            if not Confirm.ask("Do you want to sign?"):
+                return signatures
+
+            signature = _sign(metadata, missing_keys)
+            if signature:
+                signatures.append(signature)
+                break
 
 
 def _sign_one(
@@ -87,14 +407,7 @@ def _sign_one(
         console.print("Metadata is fully signed.")
         return None
 
-    keys_to_use = {}
-    # Use only those keys to sign, whose signatures are effectively missing.
-    # This also means the cli can't be used to sign beyond the threshold.
-    if not results.first.verified:
-        keys_to_use.update(results.first.unsigned)
-
-    if not results.second.verified:
-        keys_to_use.update(results.second.unsigned)
+    missing_keys = _get_missing_keys(results)
 
     _show(metadata.signed)
     _show_missing_signatures(results)
@@ -102,7 +415,7 @@ def _sign_one(
     # Loop until success
     signature = None
     while not signature:
-        signature = _sign(metadata, keys_to_use)
+        signature = _sign(metadata, missing_keys)
 
     return signature
 
@@ -122,7 +435,7 @@ def _sign(metadata: Metadata, keys: Dict[str, Key]) -> Optional[Signature]:
     choices = {}
     for keyid, key in keys.items():
         choices[keyid] = key
-        if name := key.unrecognized_fields.get("name"):
+        if name := key.unrecognized_fields.get(KEY_NAME_FIELD):
             choices[name] = key
 
     choice = Prompt.ask(
@@ -142,6 +455,39 @@ def _sign(metadata: Metadata, keys: Dict[str, Key]) -> Optional[Signature]:
     return signature
 
 
+def _load(prompt: str) -> Metadata[Root]:
+    """Prompt loop to load root from file.
+
+    Loop until success.
+    """
+    while True:
+        path = Prompt.ask(prompt)
+        try:
+            metadata = Metadata[Root].from_file(path)
+            break
+
+        except (StorageError, DeserializationError) as e:
+            console.print(f"Cannot load: {e}")
+
+    return metadata
+
+
+def _save(metadata: Metadata[Root]):
+    """Prompt loop to save root to file.
+
+    Loop until success or user exit.
+    """
+    while Confirm.ask("Save?"):
+        path = Prompt.ask("Enter path to save root", default="root.json")
+        try:
+            metadata.to_file(path)
+            console.print(f"Saved to '{path}'...")
+            break
+
+        except StorageError as e:
+            console.print(f"Cannot save: {e}")
+
+
 def _get_root_keys(root: Root) -> List[Key]:
     return [root.get_key(keyid) for keyid in root.roles[Root.type].keyids]
 
@@ -157,7 +503,7 @@ def _show(root: Root):
     key_table = Table("Role", "ID", "Name", "Signing Scheme", "Public Value")
     for key in _get_root_keys(root):
         public_value = key.keyval["public"]  # SSlibKey-specific
-        name = key.unrecognized_fields.get("name")
+        name = key.unrecognized_fields.get(KEY_NAME_FIELD)
         key_table.add_row("Root", key.keyid, name, key.scheme, public_value)
     key = _get_online_key(root)
     key_table.add_row(
@@ -245,6 +591,34 @@ def _push_signature(url: str, signature: Signature) -> str:
 @rstuf.group()  # type: ignore
 def admin2():
     """POC: alternative admin interface"""
+
+
+@admin2.command()  # type: ignore
+def update() -> None:
+    """Update root metadata and bump version.
+
+    Will ask for root metadata, public key paths, and signing key paths.
+    """
+    console.print("Root Metadata Update")
+    # Load
+    current_root_md = _load("Enter path to root to update")
+    new_root = deepcopy(current_root_md.signed)
+
+    # Update
+    _configure_expiry(new_root)
+    _configure_root_keys(new_root)
+    _configure_online_key(new_root)
+
+    # Sign and save (if changed)
+    if new_root == current_root_md.signed:
+        console.print("Not saving unchanged metadata.")
+    else:
+        new_root.version += 1
+        new_root_md = Metadata(new_root)
+        _sign_multiple(new_root_md, current_root_md.signed)
+        _save(new_root_md)
+
+    console.print("Bye.")
 
 
 @admin2.command()  # type: ignore
--- tests/unit/cli/test_admin2.py
+++ tests/unit/cli/test_admin2.py
@@ -3,12 +3,10 @@ from pathlib import Path
 import pretend
 import pytest
 from click.testing import CliRunner
-from rich.console import Console
-from rich.pretty import pprint
 from tuf.api.metadata import Metadata, Root
 
-from repository_service_tuf.cli import admin2
-from repository_service_tuf.cli.admin2 import sign
+import repository_service_tuf.cli.admin2 as admin2
+from repository_service_tuf.cli.admin2 import sign, update
 
 _FILES = Path(__file__).parent.parent.parent / "files"
 _ROOTS = _FILES / "root"
@@ -61,3 +59,52 @@ class TestSign:
 
         # Assert signature was added to metadata and sent to mock API
         assert root2.signatures[signer_keyid] == mock_api.calls[0].args[1]
+
+
+class TestUpdate:
+    def test_update(self, client, monkeypatch, patch_getpass):
+        """Exemplary root v1 update w/o signing (tested above) ."""
+        root = Metadata[Root].from_file(f"{_ROOTS / 'v1.json'}")
+        mock_save = pretend.call_recorder(lambda x: None)
+        monkeypatch.setattr(admin2, "_save", mock_save)
+
+        inputs = [
+            f"{_ROOTS / 'v1.json'}",  # Enter path to root to update:
+            "10",  # Please enter number of days from now, when root should expire:
+            "n",  # Do you want to change the expiry date? [y/n]:
+            "y",  # Do you want to change root keys? [y/n]:
+            "y",  # Do you want to change the root signature threshold? [y/n]:
+            "1",  # Please enter root signature threshold:
+            "y",  # Do you want to remove a root key? [y/n]:
+            "50d7e110ad65f3b2dba5c3cfc8c5ca259be9774cc26be3410044ffd4be3aa5f3",  # Choose key to remove [50d7e110ad65f3b2dba5c3cfc8c5ca259be9774cc26be3410044ffd4be3aa5f3/c6d8bf2e4f48b41ac2ce8eca21415ca8ef68c133b47fc33df03d4070a7e1e9cc]:
+            "y",  # Do you want to remove a root key? [y/n]:
+            "c6d8bf2e4f48b41ac2ce8eca21415ca8ef68c133b47fc33df03d4070a7e1e9cc",  # Choose key to remove [c6d8bf2e4f48b41ac2ce8eca21415ca8ef68c133b47fc33df03d4070a7e1e9cc]:
+            f"{_PEMS / 'rsa.pub'}",  # Please enter a public key path:
+            "my rsa root key",  # Please enter a key name, or press enter to continue without name:
+            "n",  # Do you want to add a root key? [y/n]:
+            "n",  # Do you want to change root keys? [y/n]:
+            "y",  # Do you want to change the online key? [y/n]:
+            f"{_PEMS / 'ec.pub'}",  # Please enter a public key path:
+            "n",  # Do you want to change the online key? [y/n]:
+            "n",  # Do you want to sign?
+        ]
+        # Assert pre-update root threshold, root keys, and online key
+        assert root.signed.roles["root"].threshold == 2
+        assert sorted(root.signed.roles["root"].keyids) == [
+            "50d7e110ad65f3b2dba5c3cfc8c5ca259be9774cc26be3410044ffd4be3aa5f3",
+            "c6d8bf2e4f48b41ac2ce8eca21415ca8ef68c133b47fc33df03d4070a7e1e9cc",
+        ]
+        assert sorted(root.signed.roles["timestamp"].keyids) == [
+            "c6d8bf2e4f48b41ac2ce8eca21415ca8ef68c133b47fc33df03d4070a7e1e9cc"
+        ]
+        result = client.invoke(update, input="\n".join(inputs))
+
+        new_root = mock_save.calls[0].args[0]
+        # Assert post-update root threshold, root keys, and online key
+        assert new_root.signed.roles["root"].threshold == 1
+        assert new_root.signed.roles["root"].keyids == [
+            "2f685fa7546f1856b123223ab086b3def14c89d24eef18f49c32508c2f60e241"
+        ]
+        assert sorted(new_root.signed.roles["timestamp"].keyids) == [
+            "50d7e110ad65f3b2dba5c3cfc8c5ca259be9774cc26be3410044ffd4be3aa5f3"
+        ]
