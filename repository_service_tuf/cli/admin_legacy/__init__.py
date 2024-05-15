430291c0050d7309b11058597be112bc1182bf0d
Kairo Araujo <kairo.araujo@testifysec.com>
2024-05-15 11:14:26 +0200

    chore: add depreaction admin-legacy and key cmds
    Add deprecation message for the commands `admin-legacy` and `key`.
    
    Signed-off-by: Kairo Araujo <kairo.araujo@testifysec.com>

--- repository_service_tuf/cli/admin_legacy/__init__.py
+++ repository_service_tuf/cli/admin_legacy/__init__.py
@@ -3,9 +3,22 @@
 #
 # SPDX-License-Identifier: MIT
 
-from repository_service_tuf.cli import rstuf
+from repository_service_tuf.cli import click, rstuf
 
 
 @rstuf.group()  # type: ignore
 def admin_legacy():
     """Administrative (Legacy) Commands"""
+    DEPRECATION_WARNING = """
+    ATTENTION:
+
+    This command is deprecated and will be removed in a future release.
+
+    This command uses the legacy keys format which are not compatible with
+    future versions of the RSTUF.
+
+    Do not use this command unless you are testing with the legacy RSTUF.
+    Consider recreating your RSTUF using the new admin commands.
+
+    """
+    click.echo(click.style(DEPRECATION_WARNING, bold=True, fg="red"))
--- repository_service_tuf/cli/key/__init__.py
+++ repository_service_tuf/cli/key/__init__.py
@@ -2,9 +2,18 @@
 #
 # SPDX-License-Identifier: MIT
 
-from repository_service_tuf.cli import rstuf
+from repository_service_tuf.cli import click, rstuf
 
 
 @rstuf.group()  # type: ignore
 def key():
     """Cryptographic Key Commands"""
+
+    DEPRECATION_WARNING = """
+    This command is deprecated and will be removed in a future release.
+
+    Please use `key` command only if you will be using the `admin-legacy`.
+    commands.
+    """
+    click.echo(click.style(DEPRECATION_WARNING, bold=True, fg="red"))
+    click.pause()
