fb8036d231d1b92799f3efee835a780d252a9bf2
Kairo de Araujo <kairo@dearaujo.nl>
2024-05-27 12:42:45 +0200

    tests: unit tests
    Signed-off-by: Kairo de Araujo <kairo@dearaujo.nl>

--- tests/unit/cli/admin/test_ceremony.py
+++ tests/unit/cli/admin/test_ceremony.py
@@ -9,12 +9,21 @@ from tests.conftest import _PAYLOADS, _PEMS, invoke_command
 class TestCeremony:
     def test_ceremony_with_custom_out(
         self,
+        monkeypatch,
         ceremony_inputs,
+        ceremony_selection,
         client,
         test_context,
         patch_getpass,
         patch_utcnow,
     ):
+
+        # public key selection options
+        monkeypatch.setattr(
+            "repository_service_tuf.cli.admin.helpers._select",
+            ceremony_selection,
+        )
+
         input_step1, input_step2, input_step3, input_step4 = ceremony_inputs
         custom_path = "file.json"
         result = invoke_command(
@@ -33,7 +42,12 @@ class TestCeremony:
         assert result.data == expected
 
     def test_ceremony_threshold_less_than_2(
-        self, ceremony_inputs, patch_getpass, patch_utcnow
+        self,
+        monkeypatch,
+        ceremony_inputs,
+        ceremony_selection,
+        patch_getpass,
+        patch_utcnow,
     ):
         input_step1, _, input_step3, input_step4 = ceremony_inputs
         input_step2 = [  # Configure Root Keys
@@ -42,15 +56,17 @@ class TestCeremony:
             "2",  # Please enter root threshold
             f"{_PEMS / 'JC.pub'}",  # Please enter path to public key
             "my rsa key",  # Please enter key name
-            "0",  # Please press 0 to add key, or remove key by entering its index  # noqa
             f"{_PEMS / 'JH.pub'}",  # Please enter path to public key
             "JimiHendrix's Key",  # Please enter key name
-            "0",  # Please press 0 to add key, or remove key by entering its index.  # noqa
             f"{_PEMS / 'JJ.pub'}",  # Please enter path to public key
             "JanisJoplin's Key",  # Please enter key name
-            "1",  # Please press 0 to add key, or remove key by entering its index. Press enter to contiue  # noqa
-            "",  # Please press 0 to add key, or remove key by entering its index. Press enter to contiue  # noqa
         ]
+
+        # public key selection options
+        monkeypatch.setattr(
+            "repository_service_tuf.cli.admin.helpers._select",
+            ceremony_selection,
+        )
 
         result = invoke_command(
             ceremony.ceremony,
@@ -69,7 +85,12 @@ class TestCeremony:
         assert "Please enter threshold above 1" in result.stdout
 
     def test_ceremony__non_positive_expiration(
-        self, ceremony_inputs, patch_getpass, patch_utcnow
+        self,
+        monkeypatch,
+        ceremony_inputs,
+        ceremony_selection,
+        patch_getpass,
+        patch_utcnow,
     ):
         _, input_step2, input_step3, input_step4 = ceremony_inputs
         input_step1 = [  # Configure online role settings and root expiration
@@ -82,6 +103,11 @@ class TestCeremony:
             "4",  # Please enter number of delegated hash bins [2/4/8/16/32/64/128/256/512/1024/2048/4096/8192/16384] (256)  # noqa
             "",  # Please enter days until expiry for root role (365)
         ]
+
+        monkeypatch.setattr(
+            "repository_service_tuf.cli.admin.helpers._select",
+            ceremony_selection,
+        )
 
         result = invoke_command(
             ceremony.ceremony,
@@ -102,6 +128,7 @@ class TestCeremony:
     def test_ceremony_api_server(
         self,
         ceremony_inputs,
+        ceremony_selection,
         monkeypatch,
         patch_getpass,
         patch_utcnow,
@@ -114,6 +141,11 @@ class TestCeremony:
         monkeypatch.setattr(ceremony, "task_status", fake_task_status)
         input_step1, input_step2, input_step3, input_step4 = ceremony_inputs
         test_context["settings"].SERVER = "http://localhost:80"
+
+        monkeypatch.setattr(
+            "repository_service_tuf.cli.admin.helpers._select",
+            ceremony_selection,
+        )
 
         result = invoke_command(
             ceremony.ceremony,
@@ -152,6 +184,7 @@ class TestCeremony:
     def test_ceremony_api_server_with_out_option(
         self,
         ceremony_inputs,
+        ceremony_selection,
         monkeypatch,
         client,
         test_context,
@@ -166,6 +199,12 @@ class TestCeremony:
         input_step1, input_step2, input_step3, input_step4 = ceremony_inputs
         test_context["settings"].SERVER = "http://localhost:80"
         custom_path = "file.json"
+
+        monkeypatch.setattr(
+            "repository_service_tuf.cli.admin.helpers._select",
+            ceremony_selection,
+        )
+
         result = invoke_command(
             ceremony.ceremony,
             inputs=input_step1 + input_step2 + input_step3 + input_step4,
@@ -203,7 +242,12 @@ class TestCeremony:
         assert "Ceremony done. 🔐 🎉. Bootstrap completed." in result.stdout
 
     def test_ceremony_online_key_one_of_root_keys(
-        self, ceremony_inputs, patch_getpass, patch_utcnow
+        self,
+        monkeypatch,
+        ceremony_inputs,
+        ceremony_selection,
+        patch_getpass,
+        patch_utcnow,
     ):
         # Test that online key cannot be one of root key's.
         input_step1, input_step2, _, input_step4 = ceremony_inputs
@@ -212,6 +256,13 @@ class TestCeremony:
             f"{_PEMS / '0d9d3d4bad91c455bc03921daa95774576b86625ac45570d0cac025b08e65043.pub'}",  # Please enter path to public key  # noqa
             "Online Key",  # Please enter a key name
         ]
+
+        # public key selection options
+        monkeypatch.setattr(
+            "repository_service_tuf.cli.admin.helpers._select",
+            ceremony_selection,
+        )
+
         result = invoke_command(
             ceremony.ceremony,
             input_step1 + input_step2 + input_step3 + input_step4,
@@ -227,41 +278,6 @@ class TestCeremony:
         assert [s["keyid"] for s in sigs_r] == [s["keyid"] for s in sigs_e]
         assert result.data == expected
         assert "Key already in use." in result.stdout
-
-    def test_ceremony_try_setting_root_keys_less_than_threshold(
-        self, ceremony_inputs, patch_getpass, patch_utcnow
-    ):
-        input_step1, _, input_step3, input_step4 = ceremony_inputs
-        # After setting one key we are trying to continue with pressing "enter"
-        # This wouldn't work as threshold is 2 and its required to set 2 keys.
-        input_step2 = [  # Configure Root Keys
-            "2",  # Please enter root threshold
-            f"{_PEMS / 'JH.pub'}",  # Please enter path to public key
-            "JimiHendrix's Key",  # Please enter key name
-            # Try continuing even though threshold is not reached.
-            "",  # Please press 0 to add key, or remove key by entering its index.  # noqa
-            "0",  # Please press 0 to add key, or remove key by entering its index. # noqa
-            f"{_PEMS / 'JJ.pub'}",  # Please enter path to public key
-            "JanisJoplin's Key",  # Please enter key name
-            "",  # Please press 0 to add key, or remove key by entering its index. Press enter to contiue  # noqa
-        ]
-        result = invoke_command(
-            ceremony.ceremony,
-            input_step1 + input_step2 + input_step3 + input_step4,
-            [],
-        )
-
-        with open(_PAYLOADS / "ceremony.json") as f:
-            expected = json.load(f)
-
-        sigs_r = result.data["metadata"]["root"].pop("signatures")
-        sigs_e = expected["metadata"]["root"].pop("signatures")
-
-        assert [s["keyid"] for s in sigs_r] == [s["keyid"] for s in sigs_e]
-        assert result.data == expected
-        # Asser that at least root_threshold number of public keys are added.
-        root_role = result.data["metadata"]["root"]["signed"]["roles"]["root"]
-        assert len(root_role["keyids"]) == root_role["threshold"]
 
 
 class TestCeremonyError:
