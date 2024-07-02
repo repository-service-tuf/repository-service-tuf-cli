cba823e2e5cb85842fddd20c63e4ba9b8a9f8283
Martin Vrachev <martin.vrachev@broadcom.com>
2024-07-02 18:10:51 +0300

    Test: "--metadata-url" higher priorty than "--in"
    Signed-off-by: Martin Vrachev <martin.vrachev@broadcom.com>

--- tests/unit/cli/admin/test_update.py
+++ tests/unit/cli/admin/test_update.py
@@ -270,6 +270,69 @@ class TestMetadataUpdate:
         assert result.data == expected
         assert fake_get_latest_md.calls == [pretend.call(fake_url, Root.type)]
 
+    def test_update_metadata_url_and_input_file(
+        self, monkeypatch, patch_getpass, patch_utcnow
+    ):
+        """Test that '--metadata-url' is with higher priority than '--in'."""
+        inputs = [
+            "n",  # Do you want to change the expiry date? [y/n] (y)
+            "n",  # Do you want to change the threshold? [y/n] (n)
+            f"{_PEMS / 'JC.pub'}",  # Please enter path to public key
+            "JoeCocker's Key",  # Please enter a key name
+            "y",  # Do you want to change the online key? [y/n] (y)
+            f"{_PEMS / 'cb20fa1061dde8e6267e0bef0981766aaadae168e917030f7f26edc7a0bab9c2.pub'}",  # Please enter path to public key  # noqa
+            "New Online Key",  # Please enter a key name
+            f"{_PEMS / 'JH.ed25519'}",  # Please enter path to encrypted private key  # noqa
+            f"{_PEMS / 'JJ.ecdsa'}",  # Please enter path to encrypted private key  # noqa
+            f"{_PEMS / 'JC.rsa'}",  # Please enter path to encrypted private key  # noqa
+        ]
+        root_md = Metadata.from_file(f"{_ROOTS / 'v1.json'}")
+        fake_get_latest_md = pretend.call_recorder(lambda *a: root_md)
+        monkeypatch.setattr(f"{MOCK_PATH}.get_latest_md", fake_get_latest_md)
+        fake_url = "http://fake-server/1.root.json"
+        args = [
+            "--metadata-url",
+            fake_url,
+            "--in",
+            f"{_ROOTS / 'v1.json'}",
+            "--dry-run",
+        ]
+
+        # selections interface
+        selection_options = iter(
+            (
+                # selection for inputs (update root keys)
+                "remove",  # add key
+                "JimiHendrix's Key",  # add key
+                "add",  # remove key
+                "continue",  # continue
+                # selection for inputs (signing root key)
+                "JimiHendrix's Key",  # select key to sign
+                "JanisJoplin's Key",  # select key to sign
+                "JoeCocker's Key",  # select key to sign
+                "continue",  # continue
+            )
+        )
+        mocked_select = pretend.call_recorder(
+            lambda *a: next(selection_options)
+        )
+
+        # public key selection options
+        monkeypatch.setattr(f"{_HELPERS}._select", mocked_select)
+
+        result = invoke_command(update.update, inputs, args)
+
+        with open(_PAYLOADS / "update.json") as f:
+            expected = json.load(f)
+
+        sigs_r = result.data["metadata"]["root"].pop("signatures")
+        sigs_e = expected["metadata"]["root"].pop("signatures")
+
+        assert [s["keyid"] for s in sigs_r] == [s["keyid"] for s in sigs_e]
+        assert result.data == expected
+        assert fake_get_latest_md.calls == [pretend.call(fake_url, Root.type)]
+        assert "Latest root version found" in result.stdout
+
     def test_update_dry_run_with_server_config_set(
         self,
         monkeypatch,
