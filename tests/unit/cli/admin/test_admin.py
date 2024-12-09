bad5017fdf452be3cac015523288b163eb558d87
Kairo Araujo <kairo.araujo@testifysec.com>
2024-12-09 17:07:21 +0100

    feat: Support headers for admin `rstuf admin -H`
    Add support to send headers (HTTP requests) to RSTUF API.
    
    Signed-off-by: Kairo Araujo <kairo.araujo@testifysec.com>

--- tests/unit/cli/admin/test_admin.py
+++ tests/unit/cli/admin/test_admin.py
@@ -8,6 +8,27 @@ class TestAdmin:
         api_server = "http://localhost:80"
         fake_settings = pretend.stub(SERVER=None)
         context = pretend.stub(obj={"settings": fake_settings})
-        _set_settings(context, api_server)
+        _set_settings(
+            context,
+            api_server,
+            headers=None,
+        )
 
         assert context.obj["settings"].SERVER == api_server
+        assert context.obj["settings"].HEADERS is None
+
+    def test_admin__set_settings_api_server_with_headers(self):
+        api_server = "http://localhost:80"
+        fake_settings = pretend.stub(SERVER=None)
+        context = pretend.stub(obj={"settings": fake_settings})
+        _set_settings(
+            context,
+            api_server,
+            headers="apikey: 1234, Content-Type: application/json",
+        )
+
+        assert context.obj["settings"].SERVER == api_server
+        assert context.obj["settings"].HEADERS == {
+            "apikey": "1234",
+            "Content-Type": "application/json",
+        }
